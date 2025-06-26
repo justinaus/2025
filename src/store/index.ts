23e44617c5baa25ee84c3c8587fc6f9b2062cdab
justinaus <justinaus2k@gmail.com>
2025-06-26 13:28:33 +0900

    chore: store todos 주석

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,6 +4,8 @@ import { createCountSlice } from './slices/count';
 import { createFishSlice } from './slices/fish';
 import { AppState } from './types';
 
+// TODO. persist, immer, DevTools
+
 export const useBoundStore = create<AppState>((...a) => ({
   // 관련된 상태들을 하나의 스토어에서 관리
   ...createCountSlice(...a),
